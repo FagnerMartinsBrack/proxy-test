bcf7c50cb30b3d62a8231f35b127e7982bce164b
Fagner Brack <github@fagnermartins.com>
2017-04-14 12:48:28 +1000

    Retry when reaching EOF and consider latency from spys.ru

--- index.js
+++ index.js
@@ -1,20 +1,31 @@
 var proxyChecker = require('proxy-checker');
+const fs = require('fs')
 var alignString = require('./src/align-string');
 var removeLineFromFile = require('./src/remove-line-from-file');
 var configuration = JSON.parse(require('fs').readFileSync('configuration.json', 'utf-8'))
 
-proxyChecker.checkProxiesFromFile(
-	'proxies.txt', {
-		url: configuration['blocked-url'],
-		regex: new RegExp(configuration['regex-for-valid-response'])
-	},
-	// Callback function to be called after the check 
-	function(host, port, ok, statusCode, err) {
-		var line = host + ':' + port;
-		console.log(alignString(line, 25) + ' => '
-			+ (ok ? 'Found a valid proxy' : 'Invalid proxy, removed!') + ' (status: ' + statusCode + ', err: ' + err + ')');
-		if ( statusCode !== 200 || !ok ) {
-			removeLineFromFile('proxies.txt', line);
-		}
-	}
-);
+const startOver = function() {
+  proxyChecker.checkProxiesFromFile(
+    'proxies.txt', {
+      url: configuration['blocked-url'],
+      regex: new RegExp(configuration['regex-for-valid-response'])
+    },
+    // Callback function to be called after the check 
+    function(host, port, ok, statusCode, err) {
+      var line = host + ':' + port;
+      console.log(alignString(line, 25) + ' => '
+        + (ok ? 'Found a valid proxy' : 'Invalid proxy, removed!') + ' (status: ' + statusCode + ', err: ' + err + ')');
+      if ( statusCode !== 200 || !ok ) {
+          removeLineFromFile('proxies.txt', line);
+      }
+      const proxyContents = fs.readFileSync('proxies.txt', { encoding: 'UTF-8' })
+      const ips = proxyContents.split('\n')
+      if (ips.indexOf(line) === ips.length - 1) {
+        console.log('Starting over for after reaching EOF at: ', line);
+        startOver()
+      }
+    }
+  );
+}
+
+startOver()
--- proxy crawlers/spys.ru.js
+++ proxy crawlers/spys.ru.js
@@ -4,12 +4,24 @@ var arrayify = function(args) {
   return [].slice.call(args);
 };
 
-var elements = arrayify(document.getElementsByClassName("spy14"));
+// Less is better
+var LATENCY_LIMIT = 5;
+var validRows = [].slice.call(document.querySelectorAll('.spy1xx, .spy1x'))
+  .filter((row) => !!row.querySelector('td').innerText.match(/[0-9]+:[0-9]+$/))
+  .filter((row) => {
+    const latency = +row.querySelectorAll('td')[5].innerText
+    return latency <= LATENCY_LIMIT
+  })
 
-var ips = elements
-  .filter((element) => !!element.innerText.match(/[0-9]+:[0-9]+$/))
-  .map((element) => element.innerText)
+var ips = validRows
+  .map((row) => {
+    // return '# Latency: ' + row.querySelectorAll('td')[5].innerText +
+    //   '<br>' + row.querySelector('td font.spy14').innerText
+    return row.querySelector('td font.spy14').innerText
+  })
   .join("<br>")
+
+console.log(ips)
 
 var openWindow = window.open('placeholder window', '_blank');
 openWindow.document.write(ips);
